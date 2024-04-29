31f5d9b3b295cb477e884abaa10a1389e16a87c1
suhana <suhanagupta2001@gmail.com>
2024-04-29 15:50:43 +0530

    commit123

--- drowsiness_yawn.py
+++ drowsiness_yawn.py
@@ -73,7 +73,7 @@ args = vars(ap.parse_args())
 
 EYE_AR_THRESH = 0.3
 EYE_AR_CONSEC_FRAMES = 30
-YAWN_THRESH = 20
+YAWN_THRESH = 7 #default was 20, then tried 10, now at 7
 alarm_status = False
 alarm_status2 = False
 saying = False
